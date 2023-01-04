d3af887a5789f3f7b5ef0f79e4e40281ce3ed50c
Lucas Godoy <75994385+lucasgweb@users.noreply.github.com>
2023-01-04 20:08:44 -0300

    Update default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <title>{{$title}} - Supera Inovação e Tecnologia</title>
+    <title>{{$title}} - Oficina Maximo</title>
     <link rel="icon" type="image/x-icon"
           href="{{asset('assets/img/favicon.ico')}}"/>
     <script data-search-pseudo-elements defer
