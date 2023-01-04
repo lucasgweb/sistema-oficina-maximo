238025454d33bc099a4de7fd6e925430504448b8
Lucas Godoy <75994385+lucasgweb@users.noreply.github.com>
2023-01-04 20:08:09 -0300

    Update auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>{{$title}} - Supera Inovação e Tecnologia</title>
+    <title>{{$title}} - Oficina Maximo</title>
     <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
     <link rel="icon" type="image/x-icon" href="{{asset('assets/img/favicon.ico')}}" />
     <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous"></script>
